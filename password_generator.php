fce915aee3cf821114b26c847aa3b3824df9d53d
Shadman Sakib <androsakib@gmail.com>
2023-09-26 19:53:40 +0600

    Create password_generator.php

---
+++ password_generator.php
@@ -0,0 +1,13 @@
+<?php
+function generatePassword($length) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[rand(0, strlen($chars) - 1)];
+    }
+    return $password;
+}
+
+$password = generatePassword(12);
+echo "Generated password: $password";
+?>
